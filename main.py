1ce7f4abe0a1b438f4c1307f978b5379f4d39487
MaximK <57616894+MaximKirpichev@users.noreply.github.com>
2023-04-22 21:35:52 +0300

    Added calculator

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import speech_recognition as sr
 import pyttsx3
 import time
+import asyncio
 
 r = sr.Recognizer()
 
 engine = pyttsx3.init()
+engine.setProperty('voice', 'ru')
 
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-
-
-def recognize_speech():
+async def recognize_speech():
     with sr.Microphone() as source:
         print("Говорите...")
         audio = r.listen(source)
 
         try:
-            text = r.recognize_google(audio, language="ru-RU")
+            text = r.recognize_google(audio, language="ru-RU").lower()
             print("Вы сказали: " + text)
 
             if "запусти таймер на" in text:
@@ -25,10 +23,11 @@ def recognize_speech():
                 engine.say(f"Таймер запущен на {duration} секунд")
                 engine.runAndWait()
 
-                time.sleep(duration)
+                await asyncio.sleep(duration)
 
-                engine.say("Таймер закончил свою работу")
+                engine.say(f"Таймер остановлен.")
                 engine.runAndWait()
+
 
             elif "останови таймер" in text:
                 engine.say("Таймер остановлен")
@@ -36,14 +35,36 @@ def recognize_speech():
 
                 time.sleep(0)
 
+            elif "сколько будет" in text:
+                value1 = int(text.split()[2])
+                value2 = int(text.split()[4])
+                operation = text.split()[3]
+
+                operations = {
+                    'умножить': lambda a, b: a * b,
+                    'х': lambda a, b: a * b,
+                    'плюс': lambda a, b: a + b,
+                    '+': lambda a, b: a + b,
+                    'минус': lambda a, b: a - b,
+                    '-': lambda a, b: a - b,
+                    'делить': lambda a, b: a / b,
+                    '/': lambda a, b: a / b,
+                }
+
+                result = operations[operation](value1, value2)
+
+                engine.say(f"Будет {result}")
+                engine.runAndWait()
+
             else:
                 engine.say("Простите, я не понимаю, что вы сказали")
                 engine.runAndWait()
 
         except:
-            engine.say("Простите, я не могу распознать вашу речь")
             engine.runAndWait()
 
 
-while True:
-    recognize_speech()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    while True:
+        loop.run_until_complete(recognize_speech())
